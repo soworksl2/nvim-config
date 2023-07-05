305377f5fdb69819534e168015264abe2ea56b6e
Jimy Aguasviva <jimy.waner11@gmail.com>
2023-07-04 21:15:09 -0400

    make the editor_configuration more readable

--- lua/editor_configuration/init.lua
+++ lua/editor_configuration/init.lua
@@ -1,12 +1,13 @@
 local go = vim.go
 
+
 -- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
--- initial important configurations
+-- Globals options
 -- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
 vim.g.mapleader = ' '
 
--- disable netrw file explorer to replace it with another better
+-- disable netrw file explorer to replace it with another better file browser
 local disable_netrw = false
 
 if disable_netrw then
@@ -14,33 +15,25 @@ if disable_netrw then
     vim.g.loaded_netrwPlugin = 1
 end
 
--- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
--- global editor configurations
--- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-
 -- setting the timeout lenght for write maps
-
 go.timeoutlen = 10000
 
 -- tabulations and automatic indentation
-
 go.tabstop = 4
 go.softtabstop = 0
 go.shiftwidth = 0
 
 go.expandtab = true
-
 go.autoindent = true
 
--- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
--- Lines numbers
--- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
-
+-- set lines numbers
 go.number = true
 go.relativenumber = true
+
 
 -- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 -- global keybinding
 -- -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
 
-vim.cmd('command! W :w') -- add the :W Ex command to save to prevent erros when fast type
+-- add the :W Ex command to save for prevent erros when fast type
+vim.cmd('command! W :w')
